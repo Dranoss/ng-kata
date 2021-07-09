2f3dcc0086e99035cdfd7869fe414957f5b76e49
Xavi <xaviregnier@gmail.com>
2021-07-09 18:45:45 +0200

    added routing to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DownComponent } from './layout/pages/down/down.component';
+import { ResetComponent } from './layout/pages/reset/reset.component';
+import { UpComponent } from './layout/pages/up/up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UpComponent},
+  {path: 'up', component: UpComponent},
+  {path: 'down', component: DownComponent},
+  {path: 'reset', component: ResetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
