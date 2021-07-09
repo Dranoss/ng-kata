6161b5e55022cc2f519a31cb2ec53f803bca51eb
Xavi <xaviregnier@gmail.com>
2021-07-09 15:57:22 +0200

    project-components-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpComponent } from './pages/up/up.component';
+import { DownComponent } from './pages/down/down.component';
+import { ResetComponent } from './pages/reset/reset.component';
+import { CounterComponent } from './dumbComponents/counter/counter.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpComponent,
+    DownComponent,
+    ResetComponent,
+    CounterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
