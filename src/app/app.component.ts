0709f4e7c5e172bf6ec7d7aca1f75634fdcc964a
Xavi <xaviregnier@gmail.com>
2021-07-10 12:47:46 +0200

    parent component using service to manage counter's state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from './services/counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   counter = 0;
+
+  constructor(private counterService: CounterService){}
+
+  ngOnInit(): void{
+    const saveCounter = localStorage.getItem('counterValue');
+    if(saveCounter){
+      this.counter = parseInt(saveCounter, 10)
+    }
+    this.counterService.counterValue.subscribe((counterValue: number) => {
+      this.counter = counterValue;
+      this.saveChange(this.counter);
+    });
+  }
+
+  saveChange(counterValue: number): void{
+    localStorage.setItem('counterValue', counterValue.toString());
+  }
 }
